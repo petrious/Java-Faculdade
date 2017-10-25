1c47f2fb863bdfffd4df32545fa931b41f6d461c
petrious <petrious@gmail.com>
2017-10-25 18:07:01 -0200

    Add files via upload

---
+++ UIGeralFX/src/controller/MainController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author PetriousL
+ */
+public class MainController implements Initializable {
+    
+    
+    
+    @FXML
+    private void btnLoginAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
